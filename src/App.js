5286124c9d0f25deef5887320d493709c5e16699
ezefferth <ezefferth96@gmail.com>
2021-03-11 16:44:20 -0300

    site ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
 
 function App() {
+
+
   return (
     <Router>
       <div>
-        <Route exact path='/' component={Home}/>
+        <Route exact path='/' component={Home} />
       </div>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { Container, Nav, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Container, Nav, Row, Col, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal'
 import CardColumns from 'react-bootstrap/CardColumns'
 import Navbar from 'react-bootstrap/Navbar'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Card from 'react-bootstrap/Card'
+import { FiInstagram } from "react-icons/fi";
+import { AiOutlineMail, AiFillGithub } from "react-icons/ai";
+
 import './Home.css'
 //https://www.digitalocean.com/community/tutorials/how-to-implement-smooth-scrolling-in-react-pt
 
@@ -11,8 +15,35 @@ import dezpassos from '../images/dezpassos.jpg'
 import jhe from '../images/jhe.jpeg'
 import zef from '../images/zef.jpeg'
 import entretextos from '../images/entretextos.jpg'
+import entretextos2 from '../images/entretextos2.jpg'
+import meninalivros from '../images/meninalivros.jpg'
+
+
 function Home() {
 
+    const [showJ, setShowJ] = useState(false);
+    const handleCloseJ = () => setShowJ(false);
+    const handleShowJ = () => setShowJ(true);
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const menuItens = document.querySelectorAll('.Nav a[href^="#"')
+
+    menuItens.forEach(item => {
+        item.addEventListener('click', scrollToRef)
+    })
+    function scrollToRef(e) {
+        e.preventDefault();
+        const element = e.target;
+        const id = element.getAttribute('href')
+        const to = document.querySelector(id).offsetTop;
+        window.scroll({
+            top: to - 80,
+            behavior: "smooth"
+        })
+    }
     return (
         <body>
             <Navbar expand='lg' className='Nav' fixed='top' bg='dark' variant='dark'>
@@ -25,12 +56,14 @@ function Home() {
                         <Nav.Link eventKey={3} href="#DPIC">Dez passos para IC</Nav.Link>
                         <Nav.Link eventKey={4} href="#QS">Quem Somos</Nav.Link>
                         <Nav.Link eventKey={5} href="#LIVROS">Livros</Nav.Link>
+                        <Nav.Link eventKey={6} href="#HELP">Ajude-nos a melhorar</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
             {
                 //sobre oque é ic <img className='img-artemaior' src={artemaior} />
             }
+
             <Jumbotron className='Jumbotron' id='home'>
                 <div className='Jumbotron-img'>
 
@@ -38,6 +71,28 @@ function Home() {
 
             </Jumbotron>
             <div >
+                <Modal show={showJ} onHide={handleCloseJ}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Email</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>jhemillyfernandes1@gmail.com</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleCloseJ}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Email</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>ezefferth96@gmail.com</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
                 <Container id='IC' className='IC'>
                     <br />
                     <Row>
@@ -63,13 +118,13 @@ function Home() {
                                 trimestres. O processo de seleção de bolsistas se encerra no mês de julho para
                                 que as bolsas sejam ativas já em agosto.
                         </p>
-                        <img src={entretextos} className='Img-Entretextos'/>
+                            <img src={entretextos} className='Img-Entretextos' />
                         </Col>
                         <Col>
-                            <iframe width="560" height="315" className='Video1-OQIC'
-                                src="https://www.youtube.com/embed/psgAt5wdgxY" frameborder="0"
+                            <iframe width="540" height="310" src="https://www.youtube.com/embed/FYMrsimeYjY" frameborder="0" className="Video1-OQIC"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen>
+
                             </iframe>
                             <p className='Text-abaixo-video1 Text'>
                                 Para concorrer ao programa de Bolsas de Iniciação Científica, basta ser
@@ -112,7 +167,9 @@ function Home() {
                     <br /><br />
                     <Row>
                         <Col>
+
                             <h1>POR QUE FAZER INICIAÇÃO CIENTÍFICA?</h1>
+                            <br />
                             <p className='Text'>
                                 A Iniciação Científica é uma experiência, que só pode ser mais bem
                                 descrita quando nós mesmos fazemos, pois, embora seja no geral uma
@@ -127,9 +184,16 @@ function Home() {
                                 aprofundar no conhecimento, te desafiam em diversas situações e fazem de
                                 sua vida acadêmica e pessoal mais rica em experiências e habilidades.
                         </p>
+                            <iframe width="540" height="315" src="https://www.youtube.com/embed/3HE8Rvof9_Y"
+                                className='Video-PQIC'
+                                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen>
 
+                            </iframe>
                         </Col>
                         <Col>
+
+                            <img src={entretextos2} className='Img-Entretexto2' />
                             <p className='Text Text-PQFIC'>
                                 Não poderia deixar de citar que as pesquisas auxiliam em muito no
                                 desenvolvimento do país e de todo o mundo. Sem elas, nenhuma tecnologia ou
@@ -146,6 +210,7 @@ function Home() {
                                 detalhadamente no projeto. E seja feliz nesse mundo, que como eu disse, só
                                 experimentando para conseguir melhor entendê-lo.
                         </p>
+
                         </Col>
                     </Row>
 
@@ -197,16 +262,38 @@ function Home() {
                                 <Card.Img variant="top" src={zef} />
                                 <Card.Title className='Card-text-title'>
                                     Ezéfferth C. A Fernandes
-                            </Card.Title>
+                                </Card.Title>
                                 <Card.Text className='Card-text'>
                                     Cofundador do Projeto
                                     Desvendando o Científico e desenvolvedor do site do Projeto Desvendando o
                                     Científico. Estudante de Engenharia da Computação pela Universidade Federal de Mato Grosso,
                                     desenvolvedor Web e Mobile nas horas vagas, esposo e pai da Isabella!
-                        </Card.Text>
+                                </Card.Text>
                             </Card.Body>
                         </Card>
+                        <Card className='Card-und' bg='dark' >
+                            <Card.Body>
+                                <iframe width="400" height="300" src="https://www.youtube.com/embed/h63ONVaxxkA"
+                                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen>
 
+                                </iframe>
+                                <Card.Title className='Card-text-title'>Redes Sociais</Card.Title>
+                                <Card.Text className='Card-text'>
+                                    @jhemillyfernandes <a href="https://www.instagram.com/jhemillyfernandes" className="Links"
+                                        target='_blank'><FiInstagram size={20} type="button" className="Icon" />
+                                    </a><AiOutlineMail size={23} className="Icon" type="button" onClick={handleShowJ} />
+
+                                </Card.Text>
+                                <Card.Text className='Card-text1'>
+                                    @ezefferth <a href="https://www.instagram.com/ezefferth" className="Links"
+                                        target='_blank'><FiInstagram size={20} type="button" className="Icon" />
+                                    </a> <AiOutlineMail size={23} className="Icon" type="button" onClick={handleShow} />
+                                    <a href="https://github.com/ezefferth" className="Links"
+                                        target='_blank'><AiFillGithub className="Icon" size={20} type="button" /></a>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </CardColumns>
                 </Container>
                 <br />
@@ -214,6 +301,139 @@ function Home() {
                 <br />
                 <br />
                 <br />
+                <Container id='LIVROS'>
+                    <Row>
+                        <Col>
+                            <h1>Vamos começar o projeto?</h1>
+                            <br />
+                            <p className='Text'>
+                                Para iniciar o projeto, muitas vezes você se sente meio perdido, não
+                                sabe nem como delimitar um tema, como construir um problema ou hipóteses,
+                                o livro de Antonio Carlos Gil - Como Elaborar Projetos de Pesquisa, te ajuda
+                                em muito nessa etapa inicial. Alias, o livro é super completo sobre
+                                metodologias, para quem tiver mais interesse.
+                                Não poderia faltar nessa etapa inicial o livro de Lakatos e Marconi -
+                                Fundamentos de Metodologia Científica. Esse livro é um dos mais completos,
+                                eu diria, contempla desde metodologias a todo o processo da pesquisa. Ele vai
+                                ser seu guia para todo o seu projeto, sempre deixe ele perto de você.
+                            </p>
+                            <p className='Text'>
+                                Não poderia faltar nessa etapa inicial o livro de Lakatos e Marconi -
+                                Fundamentos de Metodologia Científica. Esse livro é um dos mais completos,
+                                eu diria, contempla desde metodologias a todo o processo da pesquisa. Ele vai
+                                ser seu guia para todo o seu projeto, sempre deixe ele perto de você.
+                            </p>
+                            <img src={meninalivros} className='Img-Menina' />
+                            <iframe width="540" height="310" src="https://www.youtube.com/embed/j_QL9jFtlgA" frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen>
+
+                            </iframe>
+                            <br />
+                        </Col>
+                        <Col>
+                            <h1>Como começar a escrever?</h1>
+                            <br />
+                            <p className='Text'>
+                                O livro de Antônio Joaquim Severino – Metodologia do Trabalho
+                                Científico, especificamente no capítulo IV, ensina o processo de levantamentos
+                                de dados e documentos, a construção do parágrafo e aspectos técnicos do
+                                texto, ou seja, tudo que você vai precisar no desenrolar do seu projeto. O livro
+                                conta ainda com um capítulo sobre modalidades de trabalhos científicos, como
+                                o TCC, para quem tem curiosidade ou precisa saber mais sobre esse tipo de
+                                trabalho.
+                            </p>
+                            <p className='Text'>
+                                Além do mais, o livro de Elisabete Matallo Marchesini de Pádua –
+                                Metodologia de Pesquisa é outro livro ótimo e descreve o processo de
+                                pesquisa em apenas 4 etapas, ensinando como planejar a pesquisa, coletar
+                                dados, analisar dados e elaborar a escrita.
+                            </p>
+                            <p className='Text'>
+                                O livro de Marília Freitas de Campos Tozoni – Metodologia de Pesquisa
+                                é uma boa opção também para compreender mais sobre técnicas e temas de
+                                pesquisa. Seu penúltimo capítulo traz diversas informações sobre cuidados que
+                                o pesquisador iniciante deve tomar. Também, seu último capítulo aborda sobre
+                                pesquisa com temas sobre educação, para quem for da área ou tiver interesse
+                                de pesquisar.
+                            </p>
+                            <p className='Text'>
+                                Novamente, o livro de Lakatos e Marconi - Fundamentos de Metodologia
+                                Científica irá te auxiliar para o projeto e o relatório de pesquisa,
+
+                                especificamente no capítulo 10. Esse grande livro irá te orientar desde a
+                                introdução à conclusão de seu projeto.
+                            </p>
+                            <p className='Text'>
+                                Depois desses livros que vão te nortear, vamos começar um projeto?
+                                Lembre-se que com determinação, foco, e persistência, podemos conseguir
+                                muito mais do que imaginamos.
+                            </p>
+                            <br />
+                            <br />
+                            <h1>E a ABNT? O que fazer agora?</h1>
+                            <br />
+                            <p className='Text'>
+                                Calma! Primeira coisa: para um envio de projeto para o programa de
+                                Iniciação Científica ou de TCC, é preciso analisar se a sua universidade tem
+                                modelo próprio ou orientações específicas.
+                            </p>
+                            <p className='Text'>
+                                Mas mesmo com modelos ou orientações, algumas normas da ABNT
+                                (Associação Brasileira de Normas Técnicas) são solicitadas, como é o exemplo
+                                das referências. O mais interessante é que com a prática você acaba
+                                aprendendo de vez as normas.
+                            </p>
+                            <p className='Text'>
+                                Não precisa ter medo da ABNT. As normas parecem difíceis, mas com o
+                                tempo você consegue pegar o jeito. Além disso, a ABNT existe para te ajudar.
+                                Por conta dela é possível formalizar as normas técnicas de apresentações e
+                                formatações de trabalhos científicos.
+                            </p>
+                            <p className='Text'>
+                                Mas, muito cuidado com os livros, geralmente podem estar com normas
+                                desatualizadas. A ABNT constantemente modifica suas normas, fica sempre
+                                atento durante o ano. Fica de olho no site <a
+                                    href='http://www.abnt.org.br/'
+                                    target='_blank'
+                                > http://www.abnt.org.br/</a>, lá você
+                                consegue estar mais atento as mudanças das normas ABNT.
+                            </p>
+                            <p className='Text'>
+                                Além do mais, existem algumas faculdades públicas e privadas que
+                                publicam manuais que auxiliam na elaboração dos trabalhos acadêmicos
+                                baseados na ABNT. É só dar uma pesquisada e uma olhada. Podem te ajudar
+                                bastante.
+                            </p>
+                            <br />
+                        </Col>
+
+                    </Row>
+                </Container>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <Container id='HELP'>
+                    <h1>Ajude-nos a melhorar</h1>
+                    <h5>
+                        <br />
+
+                    Ajude o nosso projeto respondendo esse breve questionário: acesse <a href='https://forms.gle/cGqeu25YzzM6LT3m8' className='form'
+                            target='_blank'
+                        >aqui</a>
+                    </h5>
+                </Container>
+                <br />
+                <br />
+                <br />
+                <br />
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <span class="text-muted">Developed by @ezefferth  in React&#169; {new Date().getFullYear()}</span>
+                    </div>
+                </footer>
             </div>
         </body>
 
